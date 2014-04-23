f5133a41642bc3215d55bc0da8abaf5428088f5e
david78k <root@c11node6>
2014-04-23 14:01:11 -0400

    all

--- examples/msg/cloud/randgen/RandGen.c
+++ examples/msg/cloud/randgen/RandGen.c
@@ -5,22 +5,22 @@
 
 
 
-//Note: float variables are used. If you want to generate integer variables, just change the
+//Note: double variables are used. If you want to generate integer variables, just change the
 //return type of the function and all other types which are related to the result...
-float Constant(float mean);
-float Uniform(float mean);
-float Exponential(float mean);
-int   Poisson(float mean);
-float Pareto(float alpha);
-float Normal(float mean, float stdev);
-int	  Geometric(float p);
-float Weibull(float scale, float shape);
-float Erlang(int scale, float shape);
+double Constant(double mean);
+double Uniform(double mean);
+double Exponential(double mean);
+int   Poisson(double mean);
+double Pareto(double alpha);
+double Normal(double mean, double stdev);
+int	  Geometric(double p);
+double Weibull(double scale, double shape);
+double Erlang(int scale, double shape);
 
 //void main(void)
 void main(int argc, char **argv)
 {
-	float mean, param2, scale, shape;
+	double mean, param2, scale, shape;
 
 	//Seed is the parameter of srand (the random number generator function of C).
 	//srand takes an integer as a parameter. If this integer is the same, 
@@ -41,65 +41,72 @@ void main(int argc, char **argv)
 	mean = atof(argv[1]);
 	if(argc > 2) param2 = atof(argv[2]);
 
+	printf("RAND_MAX = %d\n", RAND_MAX);
+
 	printf("Give Mean Value:");
 	//scanf("%f",&mean);
 	
 	printf("\nConstant: %f\n",Constant(mean));
-	printf("Uniform: %f\n",Uniform(mean));
-	printf("Exponential: %f\n",Exponential(mean));
-	printf("Poisson: %d\n",Poisson(mean));
-	printf("Pareto: %d\n",Pareto(mean)); // alpha
+	printf("Uniform [0,1]: %f\n",Uniform(mean));
+	printf("Exponential [0, 1]: %f\n",Exponential(mean));
+	//printf("Poisson: %d\n",Poisson(mean));
+	printf("Pareto: %f\n",Pareto(mean)); // alpha
 	printf("Geometric: %d\n",Geometric(mean)); // p
 	if (argc > 2) {
-	printf("Normal: %f\n",Normal(mean, param2)); // mean, stdev
-	printf("Weibull: %d\n",Weibull(mean, param2)); // scale, shape
-	printf("Erlang: %d\n",Erlang(mean, param2)); // scale, shape
+		printf("Normal: %f\n",Normal(mean, param2)); // mean, stdev
+	//	printf("Weibull: %d\n",Weibull(mean, param2)); // scale, shape
+	//	printf("Erlang: %d\n",Erlang(mean, param2)); // scale, shape
 	} else
-	printf("\nMore results can be added for other functions too (Normal, Weibull, Erlang) ...\n\n");
+		printf("\nMore results can be added for other functions too (Normal, Weibull, Erlang) ...\n\n");
 }
 
 
-float Constant (float mean)
+double Constant (double mean)
 {
+	//printf("mean/5 = %f\n", mean/5);
 	return mean;
 }
 
 
-float Uniform(float mean)
+double Uniform(double mean)
 {
 	//Generate a random number between 0 and 1.
-	//REMEMBER: Always cast the oparands of a division to float, or truncation will be performed.
-	float R;
-	R = (float)rand()/(float)(RAND_MAX+1);
+	//REMEMBER: Always cast the oparands of a division to double, or truncation will be performed.
+	double R, r;	
+	r = rand();
+	R = (double)r/((double)RAND_MAX+1.0);
+//	printf("rand() = %f, R = %f\n", r, R);
+//	printf("rand()/RAND_MAX = %f\n", r/RAND_MAX);
+	//printf("rand()/5 = %f\n", r/5);
 	
 	return  2*mean*R;
 }
 
 
-float Exponential(float mean)
+double Exponential(double mean)
 {
 	//Generate a random number between 0 and 1.
-	//REMEMBER: Always cast the oparands of a division to float, or truncation will be performed.
-	float R;
-	R = (float)rand()/(float)(RAND_MAX+1);
+	//REMEMBER: Always cast the oparands of a division to double, or truncation will be performed.
+	double R;
+	R = (double)rand()/((double)RAND_MAX+1);
 	
 	//Put it as a parameter to the inverse distribution function.
 	return  -mean*log(R);
 }
 
 
-int Poisson(float mean) //Special technique required: Box-Muller method...
+int Poisson(double mean) //Special technique required: Box-Muller method...
 {
-	float R;
-	float sum = 0;
+	double R;
+	double sum = 0;
 	int i;
 	i=-1;
-	float z;
+	double z;
 
 	while(sum <=mean)
 	{
 
-		R = (float)rand()/(float)(RAND_MAX+1);
+		R = (double)rand()/(double)RAND_MAX+1;
 		z = -log(R);
 		sum+= z;
 		i++;
@@ -108,47 +115,47 @@ int Poisson(float mean) //Special technique required: Box-Muller method...
 }
 		
 
-float Pareto(float alpha)
+double Pareto(double alpha)
 {
 
-	float R;
-	R = (float)rand()/(float)(RAND_MAX+1);
-	return (float)1/(float)(pow(R,(float)1/alpha));
+	double R;
+	R = (double)rand()/((double)RAND_MAX+1);
+	return (double)1/(double)(pow(R,(double)1/alpha));
 }
 
 
-float Normal(float mean, float stdev)
+double Normal(double mean, double stdev)
 {
 
-	float R1;
-	R1 = (float)rand()/(float)(RAND_MAX+1);
-	float R2;
-	R2 = (float)rand()/(float)(RAND_MAX+1);
+	double R1;
+	R1 = (double)rand()/((double)RAND_MAX+1);
+	double R2;
+	R2 = (double)rand()/((double)RAND_MAX+1);
 	return mean + stdev*cos(2*3.14*R1)*sqrt(-log(R2));
 }
 
 
-int Geometric(float p)
+int Geometric(double p)
 {
-	float R;
-	R= (float)rand()/(float)(RAND_MAX+1);
+	double R;
+	R= (double)rand()/((double)RAND_MAX+1);
 	return (int)(log(R)/log(1-p) - 1);
 }
 
 
-float Weibull(float scale, float shape)
+double Weibull(double scale, double shape)
 {
-	float R;
-	R= (float)rand()/(float)(RAND_MAX+1);
-	return scale*pow(-log(R),(float)1/shape);
+	double R;
+	R= (double)rand()/(double)RAND_MAX+1;
+	return scale*pow(-log(R),(double)1/shape);
 }
 
 
-float Erlang(int scale, float shape)	//Special technique required...
+double Erlang(int scale, double shape)	//Special technique required...
 {
-	float R=1;
+	double R=1;
 	int i;
 	for (i=1;i<=scale;i++)
-		R = R*((float)rand()/(float)(RAND_MAX+1));
+		R = R*((double)rand()/((double)RAND_MAX+1));
 	return -shape*log(R);
 }
